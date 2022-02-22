fc075a708b539892474f306a20cc3eaad91186bd
Gabriel Soares <64909008+gabrielsoaressn@users.noreply.github.com>
2022-02-22 12:03:53 -0300

    Add files via upload
    adicionando um projeto básico em java

---
+++ hw/src/com/dio/App.java
@@ -0,0 +1,9 @@
+package com.dio;
+import com.dio.base.Order;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Order order = new Order ("helloworld");
+        System.out.println(order); 
+    }
+}
